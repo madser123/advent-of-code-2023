ea955003e441eb052300dce342529e7775517daa
Mads Jensen <madsaj10@outlook.com>
2024-07-17 11:31:07 +0200

    Correct day 1 and 2 to have both solutions in print

--- bin/src/main.rs
+++ bin/src/main.rs
@@ -18,11 +18,16 @@ use scratchcard::ScratchCards;
 use trebuchet::Trebuchet;
 
 fn day1(input: String) {
-    let result = Trebuchet::new(&input)
-        .expect("Failed to create trebuchet")
-        .get_calibration_sum();
+    let trebuchet = Trebuchet::new(&input).expect("Failed to create trebuchet");
+    let value_numeric = trebuchet
+        .get_numeric_calibration_sum()
+        .expect("Failed to get numeric calibration value");
+    println!("Numeric calibration value: {value_numeric}");
 
-    println!("Result: {result}");
+    let value = trebuchet
+        .get_calibration_sum()
+        .expect("Failed to get calibration value");
+    println!("Calibration value: {value}");
 }
 
 fn day2(input: String) {
@@ -31,16 +36,16 @@ fn day2(input: String) {
     limits.insert(Color::Green, 13);
     limits.insert(Color::Blue, 14);
 
-    let result = Game::new(&input, limits)
-        .expect("Failed to create game")
-        .get_minimum_powers_sum();
+    let game = Game::new(&input, limits).expect("Failed to create game");
+    let ids_sum = game.get_ids_sum();
+    println!("Ids sum: {ids_sum}");
 
-    println!("Result: {result}")
+    let minimum_powers = game.get_minimum_powers_sum();
+    println!("Powers sum: {minimum_powers}");
 }
 
 fn day3(input: String) {
     let schem = EngineSchematic::new(&input).expect("Failed to create schematic");
-
     let parts_sum = schem.get_parts().sum();
     println!("Part-numbers sum: {parts_sum}");
 
@@ -51,7 +56,6 @@ fn day3(input: String) {
 fn day4(input: String) {
     let mut cards = ScratchCards::from_str(&input).expect("Failed to create scratchcards!");
     let total = cards.get_points_worth().expect("Failed to calculate total");
-
     println!("Total points: {total}");
 
     let copies = cards
@@ -62,38 +66,33 @@ fn day4(input: String) {
 
 fn day5(input: String) {
     let almanac = Almanac::from_str(&input).expect("Failed to create almanac");
-    let lowest_location = almanac.get_lowest_location().expect("Lowest location not found");
 
+    let lowest_location = almanac.get_lowest_location().expect("Lowest location not found");
     println!("Lowest location: {lowest_location}");
 
     let lowest_location_seed_range = almanac
         .get_lowest_location_of_seed_ranges()
         .expect("Lowest location of ranges not found");
-
     println!("Lowest location of ranges: {lowest_location_seed_range}");
 }
 
 fn day6(input: String) {
     let races = Races::from_multiple_races(&input).expect("Failed to parse races");
     let winning_product = races.get_winning_product();
-
     println!("Winning product for multiple races: {winning_product}");
 
     let race = Races::from_single_race(&input).expect("Failed to parse race");
     let winning_product = race.get_winning_product();
-
     println!("Winning product for single race: {winning_product}");
 }
 
 fn day7(input: String) {
     let hands = Hands::<false>::from_str(&input).expect("Failed parsing hands");
     let total_winnings = hands.get_total_winnings();
-
     println!("Total winnings: {total_winnings}");
 
     let hands = Hands::<true>::from_str(&input).expect("Failed parsing hands with jokers");
     let total_joker_winnings = hands.get_total_winnings();
-
     println!("Total winnings with jokers: {total_joker_winnings}");
 }
 
@@ -101,24 +100,20 @@ fn day8(input: String) {
     let network = Network::from_str(&input).expect("Failed parsing network");
     let find = Node::from_str("ZZZ").expect("Failed parsing finde node");
     let steps = network.find_steps_required_for(&find).expect("Failed getting steps");
-
     println!("Steps from 'AAA' to 'ZZZ': {steps}");
 
     let ghost_steps = network
         .find_ghost_steps_required_for('Z')
         .expect("Failed getting ghost steps");
-
     println!("Ghost steps from '__A' to '__Z': {ghost_steps}");
 }
 
 fn day9(input: String) {
     let report = Report::from_str(&input).expect("Failed parsing report");
     let next_sum = report.get_next_values_sum();
-
     println!("Sum of next values for histories: {next_sum}");
 
     let prev_sum = report.get_prev_values_sum();
-
     println!("Sum of prev values for histories: {prev_sum}")
 }
 
--- lib/trebuchet/src/lib.rs
+++ lib/trebuchet/src/lib.rs
@@ -1,19 +1,31 @@
-use std::{num::ParseIntError, str::FromStr};
+use std::{
+    num::{ParseIntError, TryFromIntError},
+    str::FromStr,
+};
 
 const NUMBER_STRINGS: [&str; 10] = [
     "zero", "one", "two", "three", "four", "five", "six", "seven", "eight", "nine",
 ];
 
+pub type CalibrationResult<T> = Result<T, CalibrationError>;
+
 /// Error type for parsing calibration values
 #[derive(Debug)]
 pub enum CalibrationError {
-    ParseNumber(ParseIntError),
+    ParseInt(ParseIntError),
+    ConvertInt(TryFromIntError),
     NoNumbers,
 }
 
 impl From<ParseIntError> for CalibrationError {
     fn from(value: ParseIntError) -> Self {
-        Self::ParseNumber(value)
+        Self::ParseInt(value)
+    }
+}
+
+impl From<TryFromIntError> for CalibrationError {
+    fn from(value: TryFromIntError) -> Self {
+        Self::ConvertInt(value)
     }
 }
 
@@ -21,7 +33,28 @@ impl std::fmt::Display for CalibrationError {
     fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
         match self {
             Self::NoNumbers => write!(f, "No numbers found"),
-            Self::ParseNumber(int_err) => write!(f, "Failed to parse integer: {int_err:?}"),
+            Self::ParseInt(int_err) => write!(f, "Failed to parse integer: {int_err:?}"),
+            Self::ConvertInt(int_err) => write!(f, "Failed to convert integer: {int_err:?}"),
+        }
+    }
+}
+
+pub enum CalibrationNumber {
+    Alphabetic(i32),
+    Numeric(i32),
+}
+
+impl CalibrationNumber {
+    pub const fn as_int(&self) -> &i32 {
+        match self {
+            Self::Alphabetic(n) | Self::Numeric(n) => n,
+        }
+    }
+
+    pub const fn is_numeric(&self) -> bool {
+        match self {
+            Self::Numeric(_) => true,
+            Self::Alphabetic(_) => false,
         }
     }
 }
@@ -29,12 +62,32 @@ impl std::fmt::Display for CalibrationError {
 /// A calibration value representing the
 /// first and last numbers for a calibration line
 pub struct CalibrationValue {
-    number: i32,
+    numbers: Vec<CalibrationNumber>,
 }
 
 impl CalibrationValue {
-    pub const fn number(&self) -> &i32 {
-        &self.number
+    pub fn get_numeric_value(&self) -> CalibrationResult<i32> {
+        let mut numerics = self.numbers.iter().filter(|n| CalibrationNumber::is_numeric(n));
+
+        let first = numerics.next().ok_or(CalibrationError::NoNumbers)?;
+        let last = numerics.last().unwrap_or(first);
+
+        Self::parse_number(first, last)
+    }
+
+    pub fn get_value(&self) -> CalibrationResult<i32> {
+        // Get first and last number
+        let first = self.numbers.first().ok_or(CalibrationError::NoNumbers)?;
+        let last = self.numbers.last().unwrap_or(first);
+
+        Self::parse_number(first, last)
+    }
+
+    fn parse_number(first: &CalibrationNumber, last: &CalibrationNumber) -> CalibrationResult<i32> {
+        // Format numbers
+        format!("{}{}", first.as_int(), last.as_int())
+            .parse()
+            .map_err(|err: ParseIntError| err.into())
     }
 }
 
@@ -46,28 +99,26 @@ impl FromStr for CalibrationValue {
 
         // Get all number strings as numbers
         for (i, number_string) in NUMBER_STRINGS.iter().enumerate() {
-            s.match_indices(number_string)
-                .for_each(|(index, _)| assorted.push((index, i.to_string())));
+            s.match_indices(number_string).try_for_each(|(index, _)| {
+                let number = i.try_into()?;
+                assorted.push((index, CalibrationNumber::Alphabetic(number)));
+                Ok::<(), TryFromIntError>(())
+            })?;
         }
 
         // Extract all numeric characters
-        s.match_indices(char::is_numeric).for_each(|(index, number)| {
-            assorted.push((index, number.to_string()));
-        });
+        s.match_indices(char::is_numeric).try_for_each(|(index, number)| {
+            let number = number.parse()?;
+            assorted.push((index, CalibrationNumber::Numeric(number)));
+            Ok::<(), ParseIntError>(())
+        })?;
 
         // Sort by index
         assorted.sort_by_key(|(index, _)| *index);
 
-        let mut numbers = assorted.iter();
+        let numbers = assorted.into_iter().map(|(_, n)| n).collect();
 
-        // Get first and last number
-        let first = numbers.next().ok_or(CalibrationError::NoNumbers)?;
-        let last = numbers.last().unwrap_or(first);
-
-        // Format numbers
-        let number = format!("{}{}", first.1, last.1).parse()?;
-
-        Ok(Self { number })
+        Ok(Self { numbers })
     }
 }
 
@@ -85,8 +136,24 @@ impl Trebuchet {
         })
     }
 
-    pub fn get_calibration_sum(&self) -> i32 {
-        self.calibration.iter().map(CalibrationValue::number).sum()
+    pub fn get_calibration_sum(&self) -> CalibrationResult<i32> {
+        Ok(self
+            .calibration
+            .iter()
+            .map(CalibrationValue::get_value)
+            .collect::<CalibrationResult<Vec<i32>>>()?
+            .iter()
+            .sum())
+    }
+
+    pub fn get_numeric_calibration_sum(&self) -> CalibrationResult<i32> {
+        Ok(self
+            .calibration
+            .iter()
+            .map(CalibrationValue::get_numeric_value)
+            .collect::<CalibrationResult<Vec<i32>>>()?
+            .iter()
+            .sum())
     }
 }
 
@@ -99,20 +166,23 @@ mod tests {
         assert_eq!(
             CalibrationValue::from_str("a1")
                 .expect("Failed to parse calibration value")
-                .number(),
-            &11
+                .get_value()
+                .expect("Failed to get value"),
+            11
         );
         assert_eq!(
             CalibrationValue::from_str("a1b2c3d4e5f6g7h8i9")
                 .expect("Failed to parse calibration value")
-                .number(),
-            &19
+                .get_value()
+                .expect("Failed to get value"),
+            19
         );
         assert_eq!(
             CalibrationValue::from_str("a1b2c3d4e5f6g7h8i9j0nine")
                 .expect("Failed to parse calibration value")
-                .number(),
-            &19
+                .get_value()
+                .expect("Failed to get value"),
+            19
         );
     }
 
@@ -133,44 +203,51 @@ mod tests {
         assert_eq!(
             CalibrationValue::from_str("two1nine")
                 .expect("Failed to parse calibration value")
-                .number(),
-            &29
+                .get_value()
+                .expect("Failed to get value"),
+            29
         );
         assert_eq!(
             CalibrationValue::from_str("eightwothree")
                 .expect("Failed to parse calibration value")
-                .number(),
-            &83
+                .get_value()
+                .expect("Failed to get value"),
+            83
         );
         assert_eq!(
             CalibrationValue::from_str("abcone2threexyz")
                 .expect("Failed to parse calibration value")
-                .number(),
-            &13
+                .get_value()
+                .expect("Failed to get value"),
+            13
         );
         assert_eq!(
             CalibrationValue::from_str("xtwone3four")
                 .expect("Failed to parse calibration value")
-                .number(),
-            &24
+                .get_value()
+                .expect("Failed to get value"),
+            24
         );
         assert_eq!(
             CalibrationValue::from_str("4nineeightseven2")
                 .expect("Failed to parse calibration value")
-                .number(),
-            &42
+                .get_value()
+                .expect("Failed to get value"),
+            42
         );
         assert_eq!(
             CalibrationValue::from_str("zoneight234")
                 .expect("Failed to parse calibration value")
-                .number(),
-            &14
+                .get_value()
+                .expect("Failed to get value"),
+            14
         );
         assert_eq!(
             CalibrationValue::from_str("7pqrstsixteen")
                 .expect("Failed to parse calibration value")
-                .number(),
-            &76
+                .get_value()
+                .expect("Failed to get value"),
+            76
         );
     }
 }
